831684cad15a79b6cb131c0206e5eaddc1baf870
Jaimeribeiro7 <jaimeribeiro7@hotmail.com>
2025-06-09 18:51:50 -0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,1 +1,20 @@
-# URLs do projeto
+from django.contrib import admin
+from django.urls import path
+from views import (
+    login_view,
+    dashboard_view,
+    clientes_view,
+    financeiro_view,
+    materiais_view,
+    logout_view,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', login_view, name='login'),                 # Página inicial = login
+    path('dashboard/', dashboard_view, name='dashboard'),
+    path('clientes/', clientes_view, name='clientes'),
+    path('financeiro/', financeiro_view, name='financeiro'),
+    path('materiais/', materiais_view, name='materiais'),
+    path('logout/', logout_view, name='logout'),
+]
