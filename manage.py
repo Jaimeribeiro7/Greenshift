4cb06d8788cc09175825c2c57de5b73cba1ee5ca
Jaimeribeiro7 <jaimeribeiro7@hotmail.com>
2025-06-09 18:51:17 -0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,1 +1,16 @@
-# Django manage script (simplificado)
+#!/usr/bin/env python
+import os
+import sys
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Não foi possível importar o Django. Verifique se está instalado e o ambiente virtual está ativado."
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
