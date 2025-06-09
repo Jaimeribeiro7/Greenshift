f639802bf487e499117172425450e6cbc1b57ec5
Jaimeribeiro7 <jaimeribeiro7@hotmail.com>
2025-06-09 19:12:30 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,2 +1,10 @@
 ROOT_URLCONF = 'urls'
 WSGI_APPLICATION = 'wsgi.application'
+INSTALLED_APPS += [
+    'social_django',
+]
+
+AUTHENTICATION_BACKENDS = [
+    'social_core.backends.google.GoogleOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+]
