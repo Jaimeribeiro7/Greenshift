5343ec77902e3fc5581280829a4209a9ba912396
Jaimeribeiro7 <jaimeribeiro7@hotmail.com>
2025-06-10 10:53:29 -0300

    Update wsgi.py

--- greenshift/wsgi.py
+++ greenshift/wsgi.py
@@ -1,4 +1,4 @@
-import os
+hift.import os
 import sys
 
 from django.core.wsgi import get_wsgi_application
@@ -7,7 +7,7 @@ from django.core.wsgi import get_wsgi_application
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # Define o módulo de configurações padrão do Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greenshift.settings')
 
 # Obtém a aplicação WSGI
 application = get_wsgi_application()
